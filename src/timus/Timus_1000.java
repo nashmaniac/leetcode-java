ab9b028873d0889a18461e4f648d2454e128c288
Raju Ahmed Shetu <shetu2153@gmail.com>
2020-08-22 18:21:38 +0600

    updated

---
+++ src/timus/Timus_1000.java
@@ -0,0 +1,8 @@
+package timus;
+
+public class Timus_1000 {
+
+    public static void main(String[] args) {
+        Timus_1000 t = new Timus_1000();
+    }
+}
---
+++ src/timus/Timus_1264.java
@@ -0,0 +1,8 @@
+package timus;
+
+public class Timus_1264 {
+
+    public static void main(String[] args) {
+        Timus_1264 t = new Timus_1264();
+    }
+}
