d46e777030eafdf9797a2bef21d82c72471ae083
Raju Ahmed Shetu <shetu2153@gmail.com>
2020-08-22 19:01:51 +0600

    updating the first submission on timus

--- src/timus/Timus_1000.java
+++ src/timus/Timus_1000.java
@@ -1,8 +1,28 @@
 package timus;
 
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+class HelperClass {
+    public int addTwoNumber(int a , int b) {
+        return a+b;
+    }
+}
+
 public class Timus_1000 {
 
     public static void main(String[] args) {
-        Timus_1000 t = new Timus_1000();
+
+        HelperClass t = new HelperClass();
+        Scanner s = new Scanner(System.in);
+        PrintWriter out = new PrintWriter(System.out);
+
+        int a = s.nextInt();
+        int b = s.nextInt();
+
+        int out_num = t.addTwoNumber(a, b);
+
+        out.println(out_num);
+        out.flush();
     }
 }
